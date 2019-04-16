ea3e9bf463b8493a46349c1f8de649999a10068a
mouguangyi <mouguangyi@ztgame.com>
2019-04-16 18:27:01 +0800

    Update code.

--- src/kontainer/kontainer.go
+++ src/kontainer/kontainer.go
@@ -1,3 +1,9 @@
+//
+// Copyright 2019 MuGuangyi. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+//
+
 package kontainer
 
 import (
@@ -9,4 +15,8 @@ type Kontainer struct {
 
 func (k *Kontainer) Load(kervice *kervice.Kervice) error {
 	return nil
+}
+
+func (k *Kontainer) Run() {
+
 }
