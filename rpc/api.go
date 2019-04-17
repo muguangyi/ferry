958635388f3b490bafa6b58225cd5b0500ce199c
guangyi mu <muguangyi@gmail.com>
2019-04-17 22:31:23 +0800

    Add comments for API

--- rpc/api.go
+++ rpc/api.go
@@ -4,16 +4,27 @@
 
 package rpc
 
+// ICallee
 type ICallee interface {
+
+	// Bind, bind method name with corresponding function
 	Bind(name string, function interface{})
+
+	// Handling, start to handle rpc call
 	Handling()
 }
 
+// ICaller
 type ICaller interface {
+
+	// Call, call "name" method followed args and no return value
 	Call(name string, args ...interface{}) error
+
+	// CallWithResult, call "name" method followed args and has one return value
 	CallWithResult(name string, args ...interface{}) (interface{}, error)
 }
 
+// NewCallee, create a new callee
 func NewCallee() ICallee {
 	c := new(callee)
 	c.callRequest = make(chan *callRequest, 1)
@@ -22,6 +33,7 @@ func NewCallee() ICallee {
 	return c
 }
 
+// NewCaller, create a caller for target callee
 func NewCaller(c ICallee) ICaller {
 	caller := new(caller)
 	caller.callee = c.(*callee)
