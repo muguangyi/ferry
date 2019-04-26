f9c0cdd8b0fadd31ffc08635758e4205b473b72c
guangyi mu <muguangyi@gmail.com>
2019-04-26 23:40:15 +0800

    Update version file.

--- version.go
+++ version.go
@@ -1,3 +1,7 @@
+// Copyright 2019 MuGuangyi. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package unite
 
 const version string = "v0.0.1"
