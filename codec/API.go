21f61e4918c42d032e8e41b28d5cbc76810c7fc4
mouguangyi <mouguangyi@ztgame.com>
2019-05-20 12:22:30 +0800

    Add API for codec.

---
+++ codec/API.go
@@ -0,0 +1,20 @@
+// Copyright 2019 MuGuangyi. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package codec
+
+import (
+	"io"
+	"reflect"
+)
+
+// Encode target value to stream writer.
+func Encode(writer io.Writer, value interface{}) (int, error) {
+	return encode(writer, value)
+}
+
+// Decode value from stream reader.
+func Decode(reader io.Reader) (reflect.Value, error) {
+	return decode(reader)
+}
